930e1769dc13abcdc3ca514993d229d8195059f6
manavchauhan <manavc1303@gmail.com>
2026-08-10 17:49:09 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.27%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0) return 0;
+        long long low=1;
+        long long high=x;
+        long long ans=0;
+
+        while (low<=high){
+            long long mid=low+(high-low)/2;
+            long long square=mid*mid;
+            if(square==x){
+                return mid;
+            }
+            else if(square<x){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
